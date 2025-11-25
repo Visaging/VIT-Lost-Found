56a4ee739b33c9b991d1087ba77d21719bd8c25b
Visage <49914133+Visaging@users.noreply.github.com>
2025-11-25 16:58:20 +0530

    Add files via upload

---
+++ Source_Code/main.py
@@ -0,0 +1,41 @@
+import authentication as au
+import item_management as im
+import os
+
+exit=False
+
+while not exit:
+    user=au.get_cuser()
+
+    if not user:
+        os.system('cls')
+        print("----VIT Lost & Found----")
+        print("""1. Login\n2. Register\n3. Exit""")
+        choice=input("Select: ")
+
+        if choice == '1':
+            os.system('cls')
+            au.login()
+        elif choice == '2':
+            os.system('cls')
+            au.register()
+        elif choice == '3':
+            exit=True
+        else:
+            print("Invalid selection.")
+    else:
+        print(f"----DASHBOARD ({user['name']}, {user['regno']})----")
+        print("""1. Report Item\n2. Claim Item\n3. Logout""")
+        choice = input("Select: ")
+
+        if choice == '1':
+            os.system('cls')
+            im.reportitem()
+        elif choice == '2':
+            os.system('cls')
+            im.claimitem()
+        elif choice == '3':
+            os.system('cls')
+            au.logout()
+        else:
+            print("Invalid selection.")
